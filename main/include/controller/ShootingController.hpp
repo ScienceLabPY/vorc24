0cc51e8cd12a45e1849048e4751fb5d70bdd7893
Tran Dai Hao <trandaihao111333@gmail.com>
2024-08-14 01:12:44 +0700

    feat: implement ShootingController class to perform ball shooting task

---
+++ main/include/controller/ShootingController.hpp
@@ -0,0 +1,23 @@
+#ifndef SHOOTING_CONTROLLER_HPP
+#define SHOOTING_CONTROLLER_HPP
+
+#include "core/event/IEventListener.hpp"
+#include "core/event/EventTypes.hpp"
+#include "device/input/gamepad/GamepadData.hpp"
+
+namespace sl_core
+{
+    class ShootingController : public IEventListener<GamepadData>
+    {
+    public:
+        ShootingController();
+        ~ShootingController();
+
+        void onEvent(GamepadData data) override;
+    private:
+        HDMotor shootingMotor;
+        ContinuousRotationServo angleServo;
+    };
+}
+
+#endif // BALL_COLLECTOR_CONTROLLER_HPP
