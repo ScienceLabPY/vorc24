4f251a844988eac68765a4ed42c63e8c032ba89c
MinhVT22 <akacam_minhvt22@akacam.io>
2024-08-14 22:45:03 +0700

    chore: remove pseudo system design

--- pseudo/core/com/com_protocol.py
+++
@@ -1,9 +0,0 @@
-class CommunicationProtocol:
-    def initialize(self):
-        pass
-    
-    def read(self, address, register):
-        pass
-    
-    def write(self, address, register, data):
-        pass
--- pseudo/devices/input/__init__.py
+++
@@ -1,5 +0,0 @@
-from input_device import InputDevice
-
-from color_sensor import ColorSensor
-from gamepad import Gamepad
-from limit_sw import LimitSwitch
