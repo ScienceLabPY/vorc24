da6fbf24ad6f17a148fcc4a6f2118314bbcd6f11
Tran Dai Hao <trandaihao111333@gmail.com>
2024-08-15 23:03:21 +0700

    Cập nhật đọc dữ liệu các nút từ GamePad

--- main/src/sl_core/device/input/Gamepad.cpp
+++ main/src/sl_core/device/input/Gamepad.cpp
@@ -1,5 +1,4 @@
 #include <device/input/gamepad/Gamepad.hpp>
-
 using namespace sl_core;
 
 Gamepad::Gamepad() {
@@ -14,7 +13,17 @@ InputData Gamepad::read() {
     InputData data;
     // Read gamepad data and fill the InputData structure
     // This is just an example, replace with actual gamepad reading logic
-    data.gamepadData.buttonX = true;
-    data.gamepadData.buttonY = false;
+
+    data.gamepadData.buttonA = ps2x.Button(PSB_GREEN);
+    data.gamepadData.buttonB = ps2x.Button(PSB_RED);
+    data.gamepadData.buttonY = ps2x.Button(PSB_PINK);
+
+    
+    data.gamepadData.leftStickY = ps2x.Analog(PSS_LY) / 128.0f - 1.0f; // Chuyển đổi từ 0-255 sang -1.0 to 1.0
+    data.gamepadData.rightStickX = ps2x.Analog(PSS_RX) / 128.0f - 1.0f;
+
+    data.gamepadData.leftTrigger = ps2x.Analog(PSAB_L2) / 255.0f;
+    data.gamepadData.rightTrigger = ps2x.Analog(PSAB_R2) / 255.0f
+
     return data;
 }
